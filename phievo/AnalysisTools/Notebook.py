ca8d6a2e63a78f2b865aa012cb40ab64495e5952
Adrien Henry <adrien.ym.henry@gmail.com>
2017-03-24 13:43:53 -0400

    add notebook file

---
+++ phievo/AnalysisTools/Notebook.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from  ipywidgets import widgets
+from ipywidgets import interact, interactive, fixed
+from IPython.display import display,HTML,clear_output
+import os
+from phievo.AnalysisTools.Simulation import Simulation
+
+
+class Notebook:
+    """
+        Wrapper that contains both the the widgets and  the simulation results.
+        This way it is easy to update the state of the widgets when you load a
+        new simulation
+    """
+    def __init__(self):
+        self.sim = None
+        self.frame
+
+class __frame__:
+    NotImplemented
