e4351ca54f782ec46804a6f9d2f928e7e4aed818
Mathieu Hemery <mathieu.hemery@polytechnique.org>
2017-06-05 11:34:11 -0400

    Correct precedent buggy commit

--- phievo/Networks/deriv2.py
+++ phievo/Networks/deriv2.py
@@ -171,7 +171,7 @@ def all_params2C(net, prmt, print_buf, Cseed=0):
         hdr.append("#define  CONCENTRATION_SCALE %f" % prmt['langevin_noise'])
     else:
         hdr.append("#define  CONCENTRATION_SCALE 1.0")
-    if 'generation' in prmt: hdr.append("#define GENERATION %i" % prmt['generation'])
+    hdr.append("#define GENERATION %i" % prmt.get('generation',-1) )
     
     # optional generic parameters for specific C subroutines as dict or list.
     # define NFREE_PRMT is flag in Ccode that free_prmt as list is being used
