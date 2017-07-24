747fa4d80658a3101d1d4776f89946407f5152b0
Mathieu Hemery <mathieu.hemery@polytechnique.org>
2017-07-24 16:24:34 +0200

    Add test for isremovable

--- tests/test_node.py
+++ tests/test_node.py
@@ -9,8 +9,20 @@ class mock_node():
     '''A pseudo node to test check_consistency'''
     def __init__(self,type_list):
         self.types = type_list
+        self.removable = True
     def isinstance(self,name):
         return name in self.types
+
+class mock_interaction(phievo.Networks.classes_eds2.Interaction):
+    def __init__(self,list_input,list_output,net):
+        net.add_Node(self)
+        self.removable  = True
+        for input in list_input:
+            net.graph.add_edge(input,self)
+        for output in list_output:
+            net.graph.add_edge(self,output)
+    def outputs_to_delete(self,net):
+        return net.graph.successors(self)
 
 class TestNode(unittest.TestCase):
     def setUp(self):
@@ -42,5 +54,23 @@ class TestNode(unittest.TestCase):
         self.assertTrue(cc(['a','b','d'],[C,A,B]))
         self.assertFalse(cc(['a','b','a'],[C,A,B]))
 
+    def test_isremovable(self):
+        # Dummy network creation
+        self.net = phievo.Networks.classes_eds2.Network()
+        self.s1 = self.net.new_Species([['Input',0]])
+        self.s2 = self.net.new_Species([['Input',1]])
+        self.s3 = self.net.new_Species([['Output',0]])
+        self.s4 = self.net.new_Species([['Kinase']])
+        self.inter1 = mock_interaction([self.s1,self.s2],[self.s3],self.net)
+        self.inter2 = mock_interaction([self.s2],[self.s4],self.net)
+        # For now, everything should be removable
+        self.assertTrue(self.inter1.isremovable(self.net))
+        self.assertTrue(self.inter2.isremovable(self.net))
+        # Now, everything should be unremovable
+        self.s3.removable = False
+        phievo.Networks.classes_eds2.list_unremovable.append('Kinase')
+        self.assertFalse(self.inter1.isremovable(self.net))
+        self.assertFalse(self.inter2.isremovable(self.net))
+        
 if __name__ == '__main__':
     unittest.main()
