f5a385e456f67fd87fc48483287c6b561d9ae06e
Adrien Henry <adrien.ym.henry@gmail.com>
2017-08-15 18:51:26 -0400

    Add A set of to analyse the ancestry relationship between networks

--- phievo/AnalysisTools/Simulation.py
+++ phievo/AnalysisTools/Simulation.py
@@ -208,10 +208,11 @@ class Simulation:
         size = len(net.dict_types['Species'])
 
         try:
-            self.plotdata.Plot_Data(self.root+"Buffer%d"%trial_index,cell, size, nstep,list_species=list_species)
+            fig = self.plotdata.Plot_Data(self.root+"Buffer%d"%trial_index,cell, size, nstep,list_species=list_species)
         except FileNotFoundError:
             print("Make sure you have run the function run_dynamics with the correct number of trials.")
             raise
+        return fig
 
     def Plot_Profile(self,trial_index,time=0):
         """
@@ -231,11 +232,12 @@ class Simulation:
         size = len(net.dict_types['Species'])
         ncelltot = self.inits.prmt['ncelltot']
         try:
-            self.plotdata.Plot_Profile(self.root+"Buffer%d"%trial_index, ncelltot,size,time)
+            fig = self.plotdata.Plot_Profile(self.root+"Buffer%d"%trial_index, ncelltot,size,time)
         except FileNotFoundError:
             print("Make sure you have run the function run_dynamics with the correct number of trials.")
             raise
-
+        return fig
+    
     def get_genealogy(self,seed):
         return Genealogy(self.seeds[seed])
 
@@ -480,10 +482,13 @@ class Genealogy:
         self.root = seed.root
         self.restart_path = seed.restart_path
         self.seed = seed
+        self.networks = {}
 
     def sort_networks(self,verbose=False,write_pickle=True):
         """
-        Order the networks, by the label_ind in a dictionary.
+        Order the networks, by the label_ind, in a dictionary.
+        The dictonary contains the most useful information but takes last space.
+        The information dictionaries is easier to handle than the actual networks.
 
         Args:
             verbose: print information during sorting
@@ -507,20 +512,171 @@ class Genealogy:
                             par = net.parent,
                             fit = net.fitness,
                             pos = i,
+                            las = net.last_mutation if net.last_mutation else [] 
                         )
                 if verbose and (gen%100==0):
                     print("Generation\t{}/{} done.".format(gen,self.generations[-1]))
-            
+                    
         if write_pickle:
-            with open(os.path.join(self.path,"networks_info.pkl"),"wb") as pkl_networks:
-                pickle.dump(file=pkl_networks,obj=networks)
+            with open(os.path.join(self.root,"networks_info.pkl"),"wb") as pkl_networks:
+                pickle.dump(networks,pkl_networks)
         self.networks = networks
         return networks
 
     def load_sort_networks(self):
-        with open(os.path.join(self.path,"networks_info.pkl"),"wb") as pkl_networks:
-             self.networks = pickle.load(file=pkl_networks)
+        """Loads an existing network classification"""
+        with open(os.path.join(self.root,"networks_info.pkl"),"rb") as pkl_networks:
+            networks = pickle.load(pkl_networks)
+        self.networks = networks
         return networks
+
+    def search_ancestors(self,network):
+        if type(network) is int:
+            network = self.networks[network]
+        ancestors = [network]        
+        while True:    
+            try:
+                network = self.networks[network["par"]]
+                ancestors = [network] + ancestors
+            except KeyError:
+                break
+        return ancestors
+    
+    
+    def plot_front_genealogy(self,generations,extra_networks_info=[]):
+        """
+        Uses the seed plot_pareto_fronts function to display the pareto fronts.
+        In addition, the function allows to plots extra networks in the fitness plan
+        
+        Args:
+            generations: list of generation indexes
+            extra_networks_indexes: list of extra network informatino dictionaries.
+
+        """
+        from phievo.AnalysisTools import plotly_graph
+        fig = self.seed.plot_pareto_fronts(generations,with_indexes=True)
+        if extra_networks_info:
+            fit0 = [net_inf["fit"][0] for net_inf in extra_networks_info]
+            fit1 = [net_inf["fit"][1] for net_inf in extra_networks_info]
+
+            trace = plotly_graph.go.Scatter(x=fit0,y=fit1,mode = 'lines+markers',name="Extra networks",
+                                            marker= dict(size=1,color= "black",symbol="square"),
+                                            hoverinfo="text",
+                                            legendgroup = "Extra networks",
+                                            text=["net #{}\nmutation:{}".format(net_inf["ind"]," - ".join(net_inf["las"] if net_inf["las"] else [])) for net_inf in extra_networks_info]
+            )
+            fig.data.append(trace)
+            plotly_graph.py.plot(fig)
+
+    def plot_mutation_fitness_deviation(self,only_one_mutation=True):
+        """
+        Plot the deviation of fitness in the fitness space caused by a generation's mutation.
+        
+        Arg:
+           only_one_mutation (bool): If True, plot only the networks that undergone only a single mutation durign a generation.
+
+        """
+        from phievo.AnalysisTools import plotly_graph
+        dict_data = {}
+        networks = self.networks
+        for net_ind,net_inf in networks.items():
+
+            if net_inf["las"] is None or (only_one_mutation and len(net_inf["las"])!=1):
+                continue
+            label = "-".join(net_inf["las"])
+            try:
+                parent = networks[net_inf["par"]]
+            except KeyError:
+                ## Parent not provided in the dict
+                continue
+            diff = [net_inf["fit"][0]-parent["fit"][0],net_inf["fit"][1]-parent["fit"][1]]
+            data =  {"diff":diff,"label":"Net #{}\nparent #{}\nmutation: {}\nfitness change:{}".format(net_inf["ind"],parent["ind"],label,str(diff)) }
+            dict_data[label] = dict_data.get(label,[]) + [data]
+
+        plot_list = []
+        colors = palette.color_generate(len(dict_data))
+        for mut_ind,mut_name in enumerate(dict_data.keys()):
+            mutation = dict_data[mut_name]
+            x_val = [mut["diff"][0] for mut in mutation]
+            y_val = [mut["diff"][1] for mut in mutation]
+            hover_info = [mut["label"] for mut in mutation]
+            trace = plotly_graph.go.Scatter(x = x_val,y=y_val,mode = 'markers',name=mutation,
+                               marker= dict(size=14,color=colors[mut_ind]),
+                               hoverinfo="text",
+                               text=hover_info,
+            )
+            plot_list.append(trace)
+        plotly_graph.py.plot(plot_list)
+
+    def get_network_from_identifier(self,net_ind):
+        try:
+            network = self.networks[net_ind]
+        except KeyError:
+            raise KeyError("Index {} corresponds to no stored network.".format(net_ind))
+        net = self.seed.get_backup_net(network["gen"],network["pos"])
+        assert net.identifier==net_ind,"\tBug in get_network_from_identifier.\n\tThe obtained network has indec {} instead of {}.".format(net.identifier,net_ind)
+        return net
+    
+    def plot_lineage_fitness(self,line,formula="{}",highlighted_mutations = []):
+        from phievo.AnalysisTools import plotly_graph
+        generations = [net_inf["gen"] for net_inf in line]
+        fitness = [eval(formula.format(net_inf["fit"])) for net_inf in line]
+        generate_hoverinfo = lambda net:"net #{}\nmutations:{}".format(net["ind"],"-".join(net["las"] if net["las"] else [] ))
+        hover_info = [generate_hoverinfo(net_inf)  for net_inf in line]
+        colors = palette.color_generate(len(highlighted_mutations)+1)
+        trace = plotly_graph.go.Scatter(
+            x = generations,
+            y = fitness,
+            text = hover_info,
+            mode = "markers+lines",
+            marker = dict(color=colors[0]),
+        )
+        data_to_plot = [trace]
+        if highlighted_mutations:
+            dict_highlighted = {mut:plotly_graph.go.Scatter(
+                x = [],
+                y = [],
+                text = [],
+                mode = "markers",
+                marker = dict(color=colors[i+1]),
+            ) for i,mut in enumerate(highlighted_mutations)}
+
+            for net_inf in line:
+                try:
+                    ind = "-".join(net_inf["las"]) if net_inf["las"] else ""
+                    trace = dict_highlighted[ind]
+                except KeyError:
+                    continue
+                trace["x"].append(net_inf["gen"])
+                trace["y"].append(eval(formula.format(net_inf["fit"])))
+                trace["text"].append(generate_hoverinfo(net_inf))
+            data_to_plot += list(dict_highlighted.values())
+        plotly_graph.py.plot(data_to_plot)
+        
+    def plot_compare_two_networks(self,sim,net1_ind,net2_ind):
+        net1 = self.get_network_from_identifier(net1_ind)
+        net2 = self.get_network_from_identifier(net2_ind)
+        fig_format = "svg"
+        
+        fig_name = lambda xx : os.path.join(self.root,xx+"."+fig_format)
+
+        net1.draw(fig_name("net1"))
+        res = sim.run_dynamics(net1)
+        fig_profile1=sim.Plot_Profile(0,time=2999)
+        fig_profile1.savefig(fig_name("profile1"))
+        fig_time_course1=sim.Plot_TimeCourse(0,cell=8)
+        fig_time_course1.savefig(fig_name("timecourse1"))
+        
+        del fig_profile1,fig_time_course1
+
+        net2.draw(fig_name("net2"))
+        res = sim.run_dynamics(net2)
+        fig_profile2=sim.Plot_Profile(0,time=2999)
+        fig_profile2.savefig(fig_name("profile2"))
+        fig_time_course2=sim.Plot_TimeCourse(0,cell=8)
+        fig_time_course2.savefig(fig_name("timecourse2"))
+
+        del fig_profile2,fig_time_course2
 
 def pareto_plane(fitness_dico,fitnesses):
     """2d plotting subroutine of pareto_scatter"""
