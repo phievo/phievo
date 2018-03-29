e10801a73518d3bec6a3b691797a08df1c785b19
Mathieu Hemery <mathieu.hemery@polytechnique.org>
2018-03-29 10:39:16 +0200

    Add version number in __init__.py

--- phievo/__init__.py
+++ phievo/__init__.py
@@ -1,5 +1,6 @@
 __verbose__ = False
 __silent__ = False
+version = "phievo_1.1"
 from phievo.StopEvolution import test_STOP_file,create_STOP_file
 from phievo.launch_function import launch_evolution,test_project,clear_project
 import phievo.AnalysisTools
